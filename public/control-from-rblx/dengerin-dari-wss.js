d35113fff85687d24662c3624a37f2e28543327c
TIMOTHY1498 <timothyb.s@outlook.co.id>
2026-01-05 12:53:27 +0700

    Tambah fitur chat ke Roblox

--- public/control-from-rblx/dengerin-dari-wss.js
+++ public/control-from-rblx/dengerin-dari-wss.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:2210");
+const socket = new WebSocket("ws://goofy-ahh-silly.my.id//");
 
 socket.onopen = () => {
     console.log("Connected to WSS server");
@@ -12,9 +12,37 @@ socket.onmessage = (event) => {
     console.log("Raw:", event.data);
 
     const data = JSON.parse(event.data);
-    alert(
-        "From: " + data.Player +
-        "\nMessage: " + data.Message +
-        "\nTime: " + new Date(data.Time * 1000)
-    );
+    const adminAbuseView = document.getElementById("admin_abuse_view");
+    const liveChat = document.getElementById("live_chat");
+
+    if (data.Type === "AdminAbuse") {
+        adminAbuseView.innerHTML = `
+            <p>Player: ${data.Player}</p>
+            <p>Message: ${data.Message}</p>
+        `;
+        alert(`Admin Abuse Alert: ${data.Player} said: \n${data.Message}`);
+    } else if (data.Type === "LiveChat") {
+        liveChat.innerHTML += `
+            <p><strong>${data.Player}:</strong> ${data.Message}</>
+        `;
+    }
 };
+
+function toggle_ROFE() {
+    const checkbox = document.getElementById("text_filter");
+
+    socket.send(JSON.stringify({
+        Type: "ToggleFilter",
+        IsEnabled: checkbox.checked
+    }));
+}
+
+function sendMesg() {
+    const input = document.getElementById("message_input");
+    socket.send(JSON.stringify({
+        Type: "LiveChat",
+        Player: "Anonymous",
+        Message: input.value
+    }));
+    input.value = "";
+}
